95b819810ebe95196c89a505c19ae5d9dcacf4bf
hanjiwon1108 <jwjwhan1108@gmail.com>
2024-08-14 11:31:08 +0900

    기능 추가 : login 백앤드와 연동 ( axios )

--- js/log-in.js
+++ js/log-in.js
@@ -1,3 +1,36 @@
+// Axios 불러오기
+function loadScript(url, callback) {
+  const script = document.createElement("script");
+  script.src = url;
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+loadScript("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", function () {
+  // Axios가 로드 확인 유무
+  console.log("Axios loaded!");
+});
+
+function logInScript(email, password) {
+  axios
+    .post("http://192.168.0.33/api/auth/login", {
+      email: email,
+      password: password,
+    })
+    .then((response) => {
+      if (response.data.success) {
+        console.log("Login successful:", response.data); // 성공 시 데이터 처리
+        localStorage.setItem("token", response.data.token);
+      } else {
+        console.log("Login failed:", response.data.message);
+        document.getElementById("content").textContent =
+          "Login failed: " + response.data.message;
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error); // 실패 시 오류 처리
+    });
+}
+
 var button = document.getElementById("find-pw-go");
 
 // 클릭 이벤트 핸들러 추가
